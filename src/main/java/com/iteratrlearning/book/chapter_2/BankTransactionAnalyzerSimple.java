6cb536b03246e3f936b2596578259c13f7480a61
v1lliam <zotovgr@yandex.ru>
2022-09-28 01:15:57 +0300

    first commit

---
+++ src/main/java/com/iteratrlearning/book/chapter_2/BankTransactionAnalyzerSimple.java
@@ -0,0 +1,24 @@
+package main.java.com.iteratrlearning.book.chapter_2;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class BankTransactionAnalyzerSimple {
+
+    private static final String RESOURCES = "src/main/resources/";
+
+    public static void main(final String[] args) throws Exception {
+            final Path path = Paths.get(RESOURCES + "bank-data-simple.csv");
+            final List<String> lines = Files.readAllLines(path);
+            double total = 0;
+            for(final String line: lines) {
+                String[] columns = line.split(",");
+                double amount = Double.parseDouble(columns[1]);
+                total += amount;
+            }
+
+            System.out.println("The total for all transactions is " + total);
+    }
+}
